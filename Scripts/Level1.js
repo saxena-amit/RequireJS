a62a88056254918115937aa9404674cb60f88c04
saxena.amit <saxena.amit@live.com>
2015-03-16 07:49:03 -0400

    First Commit

---
+++ Scripts/Level1.js
@@ -0,0 +1,3 @@
+﻿require(['jquery-2.1.3', 'Level2'], function ($, Level2Message) {
+    alert(Level2Message);
+});
---
+++ Scripts/level2.js
@@ -0,0 +1,10 @@
+﻿define('Level2',['Level3'],function (message) {
+        return message;
+    }
+);
+
+//define('Level2DifferentFunction',['Level3'], function (level3) {
+//   //return 'Calling another Module for Require.js';
+//    return level3;
+
+//});
